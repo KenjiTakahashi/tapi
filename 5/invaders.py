d6f3a90b8afccf45b6e7092b9fccc2511cffd147
Kenji_Takahashi <wozniakk@gmail.com>
2012-04-14 21:51:30 +0200

    changed the looks and redesigned part of the code

--- 5/invaders.py
+++ 5/invaders.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Karol "Kenji Takahashi" Wozniak (C) 2012
+# GFX by David Gervais (http://forum.thegamecreators.com/?m=forum_view&t=67876&b=1)
 
 import pygame
 from pygame.locals import *
@@ -6,22 +8,59 @@ from sys import exit
 from random import randint
 
 
+def _makeInvaders():
+    invaders = list()
+    image = pygame.image.load('gfx/inv.png')
+    for i in range(3):
+        internal = list()
+        for j in range(2):
+            img = pygame.Surface((24, 24))
+            img.blit(image, (0, 0), (j * 24, i * 24, 24, 24))
+            img.set_colorkey((255, 0, 255))
+            internal.append(img)
+        invaders.append(internal)
+    return invaders
+
+
+def _makeShips():
+    ships = list()
+    image = pygame.image.load('gfx/fighter.png')
+    for i in range(3):
+        img = pygame.Surface((32, 32))
+        img.blit(image, (0, 0), (i * 32, 0, 32, 32))
+        img.set_colorkey((255, 0, 255))
+        thumb = pygame.transform.scale(img, (14, 14))
+        ships.append((img, thumb))
+    return ships
+
+
 class Alien(pygame.sprite.Sprite):
+    _invaders = _makeInvaders()
+
     def __init__(self, position, bullets, special=False):
         super(Alien, self).__init__()
         self.x, self.y = position
         self.state = 1
-        self.image = pygame.Surface((20, 20))
+        self.type = randint(0, 2)
+        self.ani = 0
+        self.tmpAni = 0
+        self.image = Alien._invaders[self.type][self.ani]
         self.rect = self.image.get_rect()
         self.rect.centerx = self.x
         self.rect.centery = self.y
         self.bullets = bullets
+        self.worth = self.type + 1
 
     def update(self):
         self.x += self.state
+        self.tmpAni += 1
+        if self.tmpAni > 7:
+            self.ani = (self.ani + 1) % 2
+            self.tmpAni = 0
+        self.image = Alien._invaders[self.type][self.ani]
         if self.x % 640 == 0:
             self.state = -self.state
-            self.y += 2
+            self.y += 4
         self.rect.centerx = self.x
         self.rect.centery = self.y
         if randint(0, 900) == 0:
@@ -39,53 +78,35 @@ class Alien(pygame.sprite.Sprite):
 
 
 class Ship(pygame.sprite.Sprite):
-    _desa = [None,
-        [],
-        [],
-        []
-    ]
+    _ships = _makeShips()
 
     def __init__(self, position, bullets):
         super(Ship, self).__init__()
         self.x, self.y = position
         self.dx, self.dy = 0, 0
-        self.image = pygame.Surface((80, 20))
-        self.image.set_colorkey((0, 0, 0))
         self.rect = None
         self.destroyed = 0
         self.bullets = bullets
         self.wait = 0
-        self.type = 1
         self.lifes = 3
-        self._setType()
+        self.setType(1)
 
-    def _setType(self):
+    def setType(self, type):
+        self.type = type
         self.dy = self.type * 10
-        if self.type == 1:
-            self.rect = pygame.draw.polygon(self.image, (250, 180, 60),
-                [(0, 20), (0, 10), (36, 10), (36, 0),
-                (41, 0), (41, 10), (80, 10), (80, 20)]
-            )
-        elif self.type == 2:
-            pass
-        elif self.type == 3:
-            pass
-        else:
-            raise ValueError()
+        self.image = Ship._ships[self.type - 1][0]
+        self.rect = self.image.get_rect()
         self.rect.centerx = self.x
         self.rect.centery = self.y
 
+    def getThumb(self):
+        return Ship._ships[self.type - 1][1]
+
     def update(self):
-        if self.destroyed:
-            self.image.fill((0, 0, 0))
-            pygame.draw.polygon(self.image, (250, 180, 60),
-                self._desa[self.destroyed]
-            )
-        else:
-            self.x += self.dx
-            self.rect.centerx = self.x
-            if self.wait > 0:
-                self.wait -= 1
+        self.x += self.dx
+        self.rect.centerx = self.x
+        if self.wait > 0:
+            self.wait -= 1
 
     def fire(self):
         if self.wait == 0:
@@ -93,7 +114,7 @@ class Ship(pygame.sprite.Sprite):
             vy = -4
             if self.type == 1:
                 self.bullets.add(Bullet(
-                    (self.rect.centerx - 3 + 3 * vx,
+                    (self.rect.centerx + 3 * vx,
                     self.rect.centery + 3 * vy),
                     vx, vy
                 ))
@@ -126,10 +147,6 @@ class Ship(pygame.sprite.Sprite):
         self.y += self.dy
         self.rect.centery = self.y
 
-    def levelUp(self):
-        self.type += 1
-        self._setType()
-
     def draw(self, screen):
         screen.blit(self.image, (self.rect.left, self.rect.top))
 
@@ -148,6 +165,8 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.centery = self.y
 
     def update(self):
+        if self.y < 0 or self.y > 454:
+            self.kill()
         self.x += self.vx
         self.y += self.vy
         self.rect.centerx = self.x
@@ -160,6 +179,10 @@ class Bullet(pygame.sprite.Sprite):
 class Scoreboard(pygame.sprite.Sprite):
     def __init__(self):
         super(Scoreboard, self).__init__()
+        pygame.init()
+        pygame.font.init()
+        self.screen = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
         self.level = 1
         self.points = 0
         self.lifes = 3
@@ -168,13 +191,18 @@ class Scoreboard(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centery = 470
         self.font = pygame.font.Font(None, 18)
-        self.image.blit(
-            self.font.render("Points: " + str(self.points), 1, (0, 0, 0)),
-            (10, 5)
-        )
-        self.image.blit(
-            self.font.render("Lifes: ", 1, (0, 0, 0)), (540, 5)
-        )
+        self.rockets = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
+        self.ship = Ship((280, 440), self.rockets)
+        self.aliens = self.doAliens()
+        self.invaders = pygame.sprite.Group(self.aliens)
+        self.update()
+
+    def doAliens(self):
+        return [
+            Alien((50 * i, 60 + 30 * j), self.bullets)
+            for i in range(15) for j in range(4)
+        ]
 
     def update(self, points=0, lifes=0):
         self.points += points
@@ -187,60 +215,51 @@ class Scoreboard(pygame.sprite.Sprite):
         self.image.blit(
             self.font.render("Lifes: ", 1, (0, 0, 0)), (540, 5)
         )
+        thumb = self.ship.getThumb()
+        for i in range(self.lifes):
+            self.image.blit(thumb, (576 + i * 14, 3))
 
-    def draw(self, screen):
-        screen.blit(self.image, (self.rect.left, self.rect.top))
+    def draw(self):
+        self.screen.blit(self.image, (self.rect.left, self.rect.top))
+
+    def run(self):
+        while True:
+            self.clock.tick(30)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == KEYDOWN and event.key == K_UP:
+                    self.ship.up()
+                if event.type == KEYUP and event.key == K_UP:
+                    self.ship.down()
+            keys = pygame.key.get_pressed()
+            self.screen.fill((60, 90, 250))
+            self.ship.drive(keys)
+            for b in self.bullets:
+                if pygame.sprite.collide_rect(b, self.ship):
+                    b.kill()
+                    self.ship.hit()
+                    self.update(lifes=1)
+                    # play a sound
+            colls = pygame.sprite.groupcollide(
+                self.invaders, self.rockets, True, True
+            )
+            if colls:
+                self.update(points=sum([a.worth for a in colls]))
+                # play a sound
+            self.draw()
+            self.invaders.update()
+            self.ship.update()
+            self.invaders.draw(self.screen)
+            self.ship.draw(self.screen)
+            self.bullets.update()
+            self.rockets.update()
+            self.bullets.draw(self.screen)
+            self.rockets.draw(self.screen)
+            pygame.display.update()
 
 
-pygame.init()
-pygame.font.init()
-screen = pygame.display.set_mode((640, 480))
-scoreboard = Scoreboard()
-
-rockets = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
-
-ship = Ship((280, 440), rockets)
-
-
-def do_aliens():
-    return [Alien((50 * i, 20 + 30 * j), bullets)
-        for i in range(15) for j in range(4)]
-aliens = do_aliens()
-invaders = pygame.sprite.Group(aliens)
-
-clock = pygame.time.Clock()
-
-while True:
-    clock.tick(30)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-        if event.type == KEYDOWN and event.key == K_UP:
-            ship.up()
-        if event.type == KEYUP and event.key == K_UP:
-            ship.down()
-    keys = pygame.key.get_pressed()
-    screen.fill((60, 90, 250))
-    ship.drive(keys)
-    for b in bullets:
-        if pygame.sprite.collide_rect(b, ship):
-            b.kill()
-            ship.hit()
-            scoreboard.update(lifes=1)
-            # play a sound
-    collides = pygame.sprite.groupcollide(rockets, invaders, True, True)
-    if collides:
-        scoreboard.update(points=len(collides))
-        # play a sound
-    scoreboard.draw(screen)
-    invaders.update()
-    ship.update()
-    invaders.draw(screen)
-    ship.draw(screen)
-    bullets.update()
-    rockets.update()
-    bullets.draw(screen)
-    rockets.draw(screen)
-    pygame.display.update()
+if __name__ == '__main__':
+    scoreboard = Scoreboard()
+    scoreboard.run()
